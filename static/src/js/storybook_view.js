6fade9df5e0a2181d35bb15a80f5e6443ba4424e
Florent Dardenne - dafl@odoo <109217759+fdardenne@users.noreply.github.com>
2022-08-09 23:07:16 +0200

    [IMP] add visual sidebar menu
    Adds the sidebar, not functional but only visual for the moment.
    The sidebar allows user to explore through stories

--- static/src/js/storybook_view.js
+++ static/src/js/storybook_view.js
@@ -1,11 +1,13 @@
 /* @odoo-module */
 
 import { registry } from "@web/core/registry";
+import { StorybookSidebar } from "./sidebar/storybook_sidebar";
 
 const { Component } = owl;
 
 class StorybookView extends Component {}
 
 StorybookView.template = "storybook.StorybookView";
+StorybookView.components = { StorybookSidebar };
 
 registry.category("actions").add("storybook_view", StorybookView);
