4ec63b6ef85d31720ffae8092cf423cd2697ddb1
Dardenne Florent (dafl) <dafl@odoo.com>
2022-09-04 19:06:26 +0200

    [IMP] sidebar: make the sidebar interactive and improve styling
    This commits makes the sidebar interactive:
    - Reads an `Object` structure to contruct itself
    - Can fold/unfold folders
    - Can click on stories, the event is passed to the parent

--- static/src/js/sidebar/storybook_sidebar.js
+++ static/src/js/sidebar/storybook_sidebar.js
@@ -1,7 +1,30 @@
 /** @odoo-module **/
 
-const { Component } = owl;
+const { Component, useState } = owl;
 
-export class StorybookSidebar extends Component {}
+export class StorybookSidebar extends Component {
+  setup() {
+    this.stories = useState(this.props.stories);
+    this.active = useState({ id: -1 });
+  }
+
+  /**
+   * Show/hide the folder
+   * @param {Object} folder - An object with a "folded" boolean property.
+   */
+  toggleFold(folder) {
+    folder.folded = !folder.folded;
+  }
+
+  /**
+   * Executes when a story is clicked in the sidebar. Update the active
+   * id and sends the information to the parent.
+   * @param {number} storyId - The id of the story that has been clicked
+   */
+  onStoryClick(storyId) {
+    this.active.id = storyId;
+    this.props.onStoryClick(storyId);
+  }
+}
 
 StorybookSidebar.template = "storybook.StorybookSidebar";
--- static/src/js/storybook_view.js
+++ static/src/js/storybook_view.js
@@ -2,12 +2,88 @@
 
 import { registry } from "@web/core/registry";
 import { StorybookSidebar } from "./sidebar/storybook_sidebar";
-
 const { Component } = owl;
-
-class StorybookView extends Component {}
+class StorybookView extends Component {
+  setup() {
+    this.stories = stories;
+  }
+  /**
+   * Load the story to the canvas
+   * @param {number} storyId - id of a story
+   */
+  loadStory(storyId) {
+    console.log(`loading story #${storyId}`);
+  }
+}
 
 StorybookView.template = "storybook.StorybookView";
 StorybookView.components = { StorybookSidebar };
 
 registry.category("actions").add("storybook_view", StorybookView);
+
+const stories = {
+  "Module 1": {
+    folders: {
+      Views: {
+        stories: [
+          { id: 1, name: "Story 1" },
+          { id: 2, name: "Story 2" },
+          { id: 3, name: "Story 3" },
+        ],
+        folded: false,
+      },
+      Widget: {
+        stories: [
+          { id: 4, name: "Story 4" },
+          { id: 5, name: "Story 5" },
+          { id: 6, name: "Story 6" },
+        ],
+        folded: false,
+      },
+    },
+    folded: false,
+  },
+  "Module 2": {
+    folders: {
+      Views: {
+        stories: [
+          { id: 7, name: "Story 1" },
+          { id: 8, name: "Story 2" },
+          { id: 9, name: "Story 3" },
+        ],
+        folded: false,
+      },
+      Widget: {
+        stories: [
+          { id: 10, name: "Story 4" },
+          { id: 11, name: "Story 5" },
+          { id: 12, name: "Story 6" },
+        ],
+        folded: false,
+      },
+    },
+    folded: false,
+  },
+  "Module 3": {
+    folders: {
+      Views: {
+        stories: [
+          { id: 13, name: "Story 1" },
+          { id: 14, name: "Story 2" },
+          { id: 15, name: "Story 3" },
+        ],
+        folded: false,
+      },
+      Widget: {
+        stories: [
+          { id: 16, name: "Story 4" },
+          { id: 17, name: "Story 5" },
+          { id: 18, name: "Story 6" },
+        ],
+        folded: false,
+      },
+    },
+    folded: false,
+  },
+};
+
