254b1b5a0b7605b644e6a7a40c8b2c68ca077aba
fdardenne <dafl@odoo.com>
2022-11-25 16:16:59 +0100

    [REF] applying linter

--- static/src/js/sidebar/sidebar.js
+++ static/src/js/sidebar/sidebar.js
@@ -4,25 +4,25 @@ import { Component } from "@odoo/owl";
 import { useStories } from "../stories";
 
 export class Sidebar extends Component {
-  setup() {
-    this.stories = useStories();
-  }
+    setup() {
+        this.stories = useStories();
+    }
 
-  /**
-   * Show/hide the folder
-   * @param {Object} folder - An object with a "folded" boolean property.
-   */
-  toggleFold(folder) {
-    folder.folded = !folder.folded;
-  }
+    /**
+     * Show/hide the folder
+     * @param {Object} folder - An object with a "folded" boolean property.
+     */
+    toggleFold(folder) {
+        folder.folded = !folder.folded;
+    }
 
-  /**
-   * Executes when a story is clicked in the sidebar. Sends the information to the parent.
-   * @param {Object} story - The story that has been clicked
-   */
-  onStoryClick(story) {
-    this.stories.setActive(story);
-  }
+    /**
+     * Executes when a story is clicked in the sidebar. Sends the information to the parent.
+     * @param {Object} story - The story that has been clicked
+     */
+    onStoryClick(story) {
+        this.stories.setActive(story);
+    }
 }
 
 Sidebar.template = "storybook.Sidebar";
