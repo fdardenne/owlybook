f80503bffb27af3f858a09ce7420cbfe25139fc5
fdardenne <dafl@odoo.com>
2022-11-25 14:17:21 +0100

    [REF] move from controller to odoo, bundle in WIP

--- __manifest__.py
+++ __manifest__.py
@@ -23,28 +23,11 @@
 
     # always loaded
     'data': [
-        'views/templates.xml',
+        'views/storybook_views.xml',
+        'views/storybook_menus.xml',
     ],
     'assets': {
-        'odoo-storybook.assets_storybook': [
-            # bootstrap
-            ('include', 'web._assets_helpers'),
-            'web/static/src/scss/pre_variables.scss',
-            'web/static/lib/bootstrap/scss/_variables.scss',
-            ('include', 'web._assets_bootstrap'),
-            'web/static/src/libs/fontawesome/css/font-awesome.css',
-            'odoo-storybook/static/src/js/**/*.scss',
-            'web/static/src/legacy/js/promise_extension.js', # required by boot.js
-            'web/static/src/boot.js', # odoo module system
-            'web/static/src/env.js', # required for services
-            'web/static/src/session.js', # expose __session_info__ containing server information
-            'web/static/lib/owl/owl.js', 
-            'web/static/lib/owl/odoo_module.js', # to be able to import "@odoo/owl"
-            # import only assets and dependencies from core
-            'web/static/src/core/utils/functions.js',
-            'web/static/src/core/browser/browser.js',
-            'web/static/src/core/registry.js',
-            'web/static/src/core/assets.js',
+        'web.assets_backend': [
             'odoo-storybook/static/src/js/**/*',
         ],
     },
--- static/src/js/main.js
+++ static/src/js/main.js
@@ -1,15 +1,16 @@
-/** @odoo-module **/
+// TODO
+// /** @odoo-module **/
 
-import { mount } from "@odoo/owl";
-import { StorybookView } from "./storybook_view";
+// import { mount } from "@odoo/owl";
+// import { StorybookView } from "./storybook_view";
 
-// The following code ensures that owl mount the component when ready.
-// `templates` contains templates contained in the bundles.
-//
-// In the mount options, it's also possible to add other interresting 
-// configuration: https://github.com/odoo/owl/blob/master/doc/reference/app.md#configuration
-import { templates } from "@web/core/assets";
+// // The following code ensures that owl mount the component when ready.
+// // `templates` contains templates contained in the bundles.
+// //
+// // In the mount options, it's also possible to add other interresting 
+// // configuration: https://github.com/odoo/owl/blob/master/doc/reference/app.md#configuration
+// import { templates } from "@web/core/assets";
 
-owl.whenReady( () => {
-    mount(StorybookView, document.body, { templates });
-});
+// owl.whenReady( () => {
+//     mount(StorybookView, document.body, { templates });
+// });
