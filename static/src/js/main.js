b8e546f46fd82295823c91736f05bc8928347030
Florent Dardenne - dafl@odoo <109217759+fdardenne@users.noreply.github.com>
2023-01-20 12:09:35 +0100

    [IMP] Move ui_playground to a Controller (#13)
    By moving the ui_playground into a controller, external users will
    be able to access the app without logging in. Additionally, this
    may also enable the integration of the ui_playground's UI via an
    iframe on other websites.
    
    This PR is far from being perfect.
    Assets in Odoo is something I find rather complex. 
    Especially when you want something very specific like having a
    owl app in a controller while having access to the backend assets.
    
    For now, it works. But I feel that we load more assets than 
    necessary. We will come back to it if the time allow us to
    do so

--- static/src/js/main.js
+++ static/src/js/main.js
@@ -1,16 +1,17 @@
-// TODO
-// /** @odoo-module **/
+/** @odoo-module **/
 
-// import { mount } from "@odoo/owl";
-// import { UIPlaygroundView } from "./ui_playground_view";
+import { mount } from "@odoo/owl";
+import { UIPlaygroundView } from "./ui_playground_view";
+import { makeEnv, startServices } from "@web/env";
+// The following code ensures that owl mount the component when ready.
+// `templates` contains templates contained in the bundles.
+//
+// In the mount options, it's also possible to add other interresting
+// configuration: https://github.com/odoo/owl/blob/master/doc/reference/app.md#configuration
+import { templates } from "@web/core/assets";
 
-// // The following code ensures that owl mount the component when ready.
-// // `templates` contains templates contained in the bundles.
-// //
-// // In the mount options, it's also possible to add other interresting 
-// // configuration: https://github.com/odoo/owl/blob/master/doc/reference/app.md#configuration
-// import { templates } from "@web/core/assets";
-
-// owl.whenReady( () => {
-//     mount(UIPlaygroundView, document.body, { templates });
-// });
+owl.whenReady(async () => {
+    const env = makeEnv();
+    await startServices(env);
+    mount(UIPlaygroundView, document.body, { templates, env });
+});
