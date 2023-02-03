ecb32ccea101b863cf554b9a4db42964436c4718
Maximilien (malb) <malb@odoo.com>
2023-02-03 17:04:45 +0100

    [IMP] Adding white square
    This PR adds a white square so that the separation between the sidebar and the canvas is more clear. In addition to that a little refactor of color has been done.
    Also this PR adds a story for the list view.

---
+++ static/src/stories/tree/tree.stories.js
@@ -0,0 +1,46 @@
+/** @odoo-module */
+
+import { registry } from "@web/core/registry";
+
+const serverData = {
+    models: {
+        order: {
+            fields: {
+                image_url: { string: "Image", type: "char" },
+                description: { string: "Description", type: "char" },
+            },
+            records: [
+                {
+                    id: 1,
+                    image_url: "",
+                    description: "A nice description",
+                },
+                {
+                    id: 2,
+                    image_url: "",
+                    description: "A second nice description",
+                },
+            ],
+        },
+    },
+    views: {},
+};
+
+const treeExample = {
+    title: "Tree example",
+    model: "order",
+    viewType: "list",
+    arch: `<tree>
+    <field name="description"/>
+    <field name="image_url"/>
+</tree>`,
+    serverData,
+};
+
+export const TreeStories = {
+    title: "Tree",
+    module: "web",
+    stories: [treeExample],
+};
+
+registry.category("stories").add("ui_playground.tree", TreeStories);
