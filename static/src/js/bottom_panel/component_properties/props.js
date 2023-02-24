cf2ac1cf904de606510bc07de0fd0f24ece3b02c
fdardenne <dafl@odoo.com>
2023-02-24 16:35:27 +0100

    [IMP] show Object and Array in props
    This commit introduces objects and arrays in component properties
    tab.
    
    It shows object and array element recursively, user can also fold
    and unfold elements.

--- static/src/js/bottom_panel/component_properties/props.js
+++ static/src/js/bottom_panel/component_properties/props.js
@@ -2,16 +2,25 @@
 
 import { Component } from "@odoo/owl";
 import { useStories } from "../../stories";
-
+import { ObjectRenderer } from "../../components/object_renderer/object_renderer";
 export class Props extends Component {
     static template = "ui_playground.properties";
+    static components = { ObjectRenderer };
 
     setup() {
         this.stories = useStories();
     }
 
     propertyType(props) {
-        const str = props?.type?.name || typeof props.value;
+        const propsValidationType = props?.type?.name;
+
+        if (propsValidationType) {
+            return props.type.name;
+        }
+        if (Array.isArray(props.value)) {
+            return "Array";
+        }
+        const str = typeof props.value;
         return str.charAt(0).toUpperCase() + str.slice(1);
     }
 
