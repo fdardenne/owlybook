5294c86ba12d5824d51e6e7e4f33818708201117
fdardenne <dafl@odoo.com>
2023-03-03 12:58:58 +0100

    [FIX] miscellianous
    This PR fixes some misc issues:
    
    - Disables the delete button for component props
    - Only disallow focus on the canvas when the bottom panel is edited
    - Linting
    - the default props from Component are taken into account
    
    This PR also adds the CodeEditor story

--- static/src/js/canvas/canvas.js
+++ static/src/js/canvas/canvas.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import { Component, useEffect, useRef } from "@odoo/owl";
+import { Component, useEffect } from "@odoo/owl";
 import { ComponentRenderer } from "./component_renderer";
 import { ArchRenderer } from "./arch_renderer";
 import { useStories } from "../stories";
@@ -15,11 +15,19 @@ export class Canvas extends Component {
         // We don't want the user to be interrupted by Autofocus made by Views or Component when the
         // user edit props or the arch. This is the only way I found to avoid the focus as the
         // useAutofocus function is not patchable.
+        // TODO: find a better solution ...
         useEffect(
             (canvasElement) => {
                 const realFocusFunction = HTMLElement.prototype.focus;
                 HTMLElement.prototype.focus = function (params) {
-                    if (!document.querySelector(".o_canvas_sheet").contains(this)) {
+                    // Enable focus only when the bottom panel is not edited and the focus come from
+                    // the canvas
+                    if (
+                        !document.querySelector(".o_canvas_sheet").contains(this) ||
+                        !document
+                            .querySelector(".o_ui_playground_panel")
+                            .contains(document.activeElement)
+                    ) {
                         return realFocusFunction.bind(this)(params);
                     }
                 };
