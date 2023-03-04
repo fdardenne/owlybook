74f282d1141b81502f6b7c9f9e37cf997aa40843
fdardenne <dafl@odoo.com>
2023-03-04 12:32:10 +0100

    [FIX] fix errors from services & restore tooltip
    Previously, localizationService, companyService and menuService caused
    some errors because there is no web session information for our controller.
    
    By faking the services, we get rid of the errors.
    
    This commit also restore tooltips and the popovers.

--- static/src/js/main.js
+++ static/src/js/main.js
@@ -3,6 +3,11 @@
 import { mount } from "@odoo/owl";
 import { UIPlaygroundView } from "./ui_playground_view";
 import { makeEnv, startServices } from "@web/env";
+import { registry } from "@web/core/registry";
+import {
+    fakeCompanyService,
+    makeFakeLocalizationService,
+} from "@web/../tests/helpers/mock_services";
 
 // The following code ensures that owl mount the component when ready.
 // `templates` contains templates contained in the bundles.
@@ -11,9 +16,27 @@ import { makeEnv, startServices } from "@web/env";
 // configuration: https://github.com/odoo/owl/blob/master/doc/reference/app.md#configuration
 import { templates } from "@web/core/assets";
 
+const serviceRegistry = registry.category("services");
+
 owl.whenReady(async () => {
     const env = makeEnv();
+
+    serviceRegistry.add("localization", makeFakeLocalizationService(), { force: true });
+    serviceRegistry.add("company", fakeCompanyService, { force: true });
+    serviceRegistry.add(
+        "menu",
+        {
+            start() {
+                return {};
+            },
+        },
+        { force: true }
+    );
+
     await startServices(env);
+
+    // @ts-ignore
     owl.Component.env.session = {};
+    // @ts-ignore
     mount(UIPlaygroundView, document.body, { templates, env });
 });
