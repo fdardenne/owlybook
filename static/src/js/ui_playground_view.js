8b0200b6801ed8872786d8e27a7e512ea39cf803
fdardenne <dafl@odoo.com>
2023-03-05 01:29:51 +0100

    [REF] use router service for url management
    This commit replace the current url management by the router service.
    By doing this:
    - The code is simplified and more robust
    - The QUnit test can fake the router service so that the url is
      not really modified during tests.

--- static/src/js/ui_playground_view.js
+++ static/src/js/ui_playground_view.js
@@ -6,26 +6,25 @@ import { Panel } from "./bottom_panel/panel";
 import { Component, onMounted } from "@odoo/owl";
 import { setupStories } from "./stories";
 import { registry } from "@web/core/registry";
+import { useBus, useService } from "@web/core/utils/hooks";
 import { MainComponentsContainer } from "@web/core/main_components_container";
 
 export class UIPlaygroundView extends Component {
     setup() {
-        this.stories = setupStories();
-        this.setStoryFromUrl();
+        this.router = useService("router");
+        this.stories = setupStories(this.router);
         onMounted(this.onMounted);
+        useBus(this.env.bus, "ROUTE_CHANGE", this.setStoryFromUrl);
     }
 
     onMounted() {
-        window.addEventListener("hashchange", (ev) => {
-            this.setStoryFromUrl();
-        });
+        this.setStoryFromUrl();
     }
 
     setStoryFromUrl() {
-        const hash = window.location.hash;
-        if (hash !== "") {
-            const params = this.stories.parseParams(hash.slice(1));
-            this.stories.setActive(this.stories.getStoryByName(params));
+        const hash = this.router.current.hash;
+        if (hash.title && hash.folder && hash.module) {
+            this.stories.setActive(this.stories.getStoryByDescription(hash));
         }
     }
 }
