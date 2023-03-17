2c2d0c57118d3493c2ef75525eae27426055e86d
Maximilien (malb) <malb@odoo.com>
2023-03-17 11:12:51 +0100

    [IMP] Adding tree stories
    This PR adds stories for the tree view so that the side bar and usability of owlybook is more complete

---
+++ static/src/stories/tree/aggregate.stories.js
@@ -0,0 +1,41 @@
+/** @odoo-module */
+
+import { registry } from "@web/core/registry";
+
+const serverData = {
+    models: {
+        order: {
+            fields: {
+                int_field: { string: "Integer", type: "integer" },
+                description: { string: "Description", type: "char" },
+            },
+            records: [
+                {
+                    id: 1,
+                    int_field: 0,
+                    description: 'Try to do a formula in the integer field like "=2*3" or "=6/2"',
+                },
+            ],
+        },
+    },
+    views: {},
+};
+
+const AggregateExample = {
+    title: "List Aggregate",
+    model: "order",
+    viewType: "list",
+    arch: `<tree editable="bottom">
+    <field name="description"/>
+    <field name="int_field"/>
+</tree>`,
+    serverData,
+};
+
+export const AggregateStories = {
+    title: "List",
+    module: "web",
+    stories: [AggregateExample],
+};
+
+registry.category("stories").add("owlybook.Aggregate", AggregateStories);
---
+++ static/src/stories/tree/sum.stories.js
@@ -0,0 +1,45 @@
+/** @odoo-module */
+
+import { registry } from "@web/core/registry";
+
+const serverData = {
+    models: {
+        order: {
+            fields: {
+                int_field: { string: "Sum", type: "integer" },
+                int_field_avg: { string: "Average", type: "integer" },
+                description: { string: "Description", type: "char" },
+            },
+            records: [
+                {
+                    id: 1,
+                    int_field: 0,
+                    int_field_avg: 0,
+                    description:
+                        "Every time you put an number in the Sum or Average field, you will see the sum and average will be computed",
+                },
+            ],
+        },
+    },
+    views: {},
+};
+
+const SumExample = {
+    title: "List Sum and Average",
+    model: "order",
+    viewType: "list",
+    arch: `<tree editable="bottom">
+    <field name="description"/>
+    <field name="int_field" sum="Total"/>
+    <field name="int_field_avg" avg="Average"/>
+</tree>`,
+    serverData,
+};
+
+export const SumStories = {
+    title: "List",
+    module: "web",
+    stories: [SumExample],
+};
+
+registry.category("stories").add("owlybook.Sum", SumStories);
